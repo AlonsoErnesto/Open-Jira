1c1271797cd8f9cd570ce3b93d8aa42bd0e5c0cf
AlonsoErnesto <ernesto134alonso123@gmail.com>
2023-05-21 21:13:47 -0500

    Entry Button

--- components/UI/index.ts
+++ components/UI/index.ts
@@ -3,3 +3,5 @@ export { default as Sidebar} from './Sidebar';
 
 export { default as EntryList } from './EntryList';
 export { default as EntryCard } from './EntryCard';
+
+export { default as NewEntry} from './NewEntry';
